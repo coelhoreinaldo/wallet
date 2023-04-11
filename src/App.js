9df28d2e6f02c92aebea869ad26bb55e81372f98
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-11 15:17:47 -0300

    ✨ feat: Req 1.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,3 +3,14 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+// {
+//   user: {
+//     email: '', // string que armazena o email da pessoa usuária
+//   },
+//   wallet: {
+//     currencies: [], // array de string
+//     expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
+//     editor: false, // valor booleano que indica de uma despesa está sendo editada
+//     idToEdit: 0, // valor numérico que armazena o id da despesa que esta sendo editada
+//   }
+// }
