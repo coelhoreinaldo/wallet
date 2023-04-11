c6ed62ccae4d8210acac14262cf14e67456216de
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-11 15:55:05 -0300

    ✨ feat: Req 1, redux config

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+import { ADD_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  email: '', // string que armazena o email da pessoa usuária
+};
+
+const userReducer = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case ADD_EMAIL:
+    return ({
+      ...state,
+      email: payload,
+    });
+
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
