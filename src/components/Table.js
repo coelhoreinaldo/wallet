01950f93cc1b4bcc33b56e95929aed389b3908da
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-12 09:55:35 -0300

    ✨ feat: req 6, creating table

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,33 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <>
+        <div>Table</div>
+        <table border="1">
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          <tr>
+            <td>Descrição</td>
+            <td>Tag</td>
+            <td>Método de pagamento</td>
+            <td>Valor</td>
+            <td>Moeda</td>
+            <td>Câmbio utilizado</td>
+            <td>Valor convertido</td>
+            <td>Moeda de conversão</td>
+            <td>Editar/Excluir</td>
+          </tr>
+        </table>
+      </>
     );
   }
 }
